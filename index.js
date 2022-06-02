f88d0da22622a813c4c3377a4a15ddba30d0f6d0
Malejo01 <lizarragamauroalejandro@gmail.com>
2022-06-02 00:46:15 -0300

    modificando botones

--- index.js
+++ index.js
@@ -15,39 +15,60 @@ let carrito = [
     productos[0], productos[3], productos[6]
 ];
 
-//Clicks
+//Eventos
 //boton admin
 let botonAdmin = document.getElementById("botonAdmin")
 botonAdmin.addEventListener ("click", () => adminClick ())
+botonAdmin.addEventListener ("mouseover", () => botonAdminPrendido)
+//botonAdmin.addEventListener ("mouseout", () => botonAdminApagado)
 
 //boton cliente
 let botonCliente = document.getElementById("botonCliente")
 botonCliente.addEventListener ("click", () => clienteClick ())
+botonCliente.addEventListener ("mouseover", () => botonClientePrendido)
+//botonCliente.addEventListener ("mouseout", () => botonClientePrendido)
+
+//------------------------------------------------------------Funciones
+
+function botonAdminPrendido () {
+    botonAdmin.className ="botonprendido"
+}
+function botonAdminApagado () {
+    botonAdmin.className ="botonapagado"
+}
 
 function adminClick() {
-    var divCliente = document.getElementById("menuCliente");
+    let divCliente = document.getElementById("menuCliente");
     divCliente.style.display = "none";
     //Buscar div menuLogin
-    var divLogin = document.getElementById("cosasDeAdmin");
+    let divLogin = document.getElementById("cosasDeAdmin");
     //Mostrar div menuLogin
     divLogin.style.display = "block";
 }
 
+
+function botonClientePrendido () {
+    botonCliente.className ="botonprendido"
+}
+function botonClienteApagado () {
+    botonCliente.className ="botonapagado"
+}
+
 function clienteClick() {
     //Buscar div menuLogin
-    var divLogin = document.getElementById("cosasDeAdmin");
+    let divLogin = document.getElementById("cosasDeAdmin");
     //Ocultar div menuLogin
     divLogin.style.display = "none";
     //Buscar div menuCliente
-    var divCliente = document.getElementById("menuCliente");
+    let divCliente = document.getElementById("menuCliente");
     //Mostrar div menuCliente
     divCliente.style.display = "block";
 }
 
 function loginClick() {
     //Obtener usuario y contraseña
-    var usuario = document.getElementById("usuario").value;
-    var contrasena = document.getElementById("contrasena").value;
+    let usuario = document.getElementById("usuario").value;
+    let contrasena = document.getElementById("contrasena").value;
     if (usuario == "admin" && contrasena == "admin") {
         //Todo: Mostrar menu de admin y esconder menu de login
         var divLogin = document.getElementById("menuLogin");
@@ -62,7 +83,7 @@ function loginClick() {
 
 function adminOpcionClick() {
     //Obtener opcion elegida
-    var opcion = document.getElementById("opcionAdminElegida").value;
+    let opcion = document.getElementById("opcionAdminElegida").value;
     switch (opcion) {
         case "1": //Mostrar carrito
             mostrarProductos()
@@ -76,6 +97,7 @@ function adminOpcionClick() {
     }
 }
 
+
 function crearElementoLista(producto) {
     let li = document.createElement("li");
     li.innerHTML = producto.id + " - " + producto.tipo + " - " + producto.nombre + " - " + "$" + producto.precio;
@@ -84,7 +106,7 @@ function crearElementoLista(producto) {
 
 function mostrarProductos() {
     //Obtenemos el elemento 
-    var listaDOM = document.getElementById("lista");
+    let listaDOM = document.getElementById("lista");
     //Borramos el contenido
     listaDOM.innerHTML = "";
     for (let i = 0; i < productos.length; i++) {
@@ -97,7 +119,7 @@ function mostrarProductos() {
 
 function mostrarCarrito() {
     //Obtenemos el elemento
-    var listaDOM = document.getElementById("lista");
+    let listaDOM = document.getElementById("lista");
     //Borramos el contenido
     listaDOM.innerHTML = "";
     for (let i = 0; i < carrito.length; i++) {
